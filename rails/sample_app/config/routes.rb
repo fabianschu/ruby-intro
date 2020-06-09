2cf40157386480bf7da26a74b771f310a6f55ba3
Fabian Scherer <48454910+fabianschu@users.noreply.github.com>
2020-06-09 16:05:51 +0200

    finished chapter 5

--- rails/sample_app/config/routes.rb
+++ rails/sample_app/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/about' => 'static_pages#about'
   get '/contact' => 'static_pages#contact'
   get '/home' => 'static_pages#home'
+  get '/signup' => 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
